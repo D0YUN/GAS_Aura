52a45836a00ed68affb702ac01eb4a0422c217c5
D0YUN <191944956+D0YUN@users.noreply.github.com>
2025-07-16 10:10:37 +0900

    [03-20 ~ 03-21] GAS in Multiplayer ~ Constructing the ASC and AS

--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -1,6 +1,18 @@
 #include "Player/AuraPlayerState.h"
 
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+
 AAuraPlayerState::AAuraPlayerState()
 {
 	SetNetUpdateFrequency(100.f);
+
+	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+
+	AttributeSet = CreateDefaultSubobject<UAttributeSet>("AttributeSet");
+}
+
+UAbilitySystemComponent* AAuraPlayerState::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
 }
